ca572b40fd3102f1ce9894012a2cc3f7725370cd
unknown <stanislav.danylov@gmail.com>
2020-09-01 20:03:38 +0300

    create forms

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const loadform = async () => {
 	const html = await (await fetch('https://alina-osyka.github.io/test/resources/form.html')).text()
 	document.body.appendChild(document.createElement('main')).innerHTML = html
 }
+loadform()
