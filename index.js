42baf7a4d06402d3fc2b4b7fd34a16d139808930
unknown <stanislav.danylov@gmail.com>
2020-09-01 21:00:18 +0300

    event handler

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const loadform = async () => {
-	const html = await (await fetch('https://alina-osyka.github.io/test/resources/form.html')).text()
-	document.body.appendChild(document.createElement('main')).innerHTML = html
+  const html = await (await fetch('https://alina-osyka.github.io/test/resources/form.html')).text()
+	document.body
+	  .appendChild(document.createElement('main'))
+	    .innerHTML = html
+  const [login, password, avatar] = ['login', 'password', 'avatar'].map((id) => document.getElementById(id))
+  login.onchange = function(event) {
+  	fetch(`http://localhost:3000/users?login=${event.target.value}`)
+  	  .then((response) => response.json())
+  	  .then((response) => {
+  	  	if (response.length === 0) {
+  	  	  event.target.style.color = 'green'
+  	  	} else {
+  	  	  event.target.style.color = 'red'
+  	  	}
+  	  })
+  }
 }
 loadform()
