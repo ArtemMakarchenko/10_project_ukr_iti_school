9715d88dee8ab897ca63741dc3041169c2574ccd
unknown <stanislav.danylov@gmail.com>
2020-09-01 21:13:45 +0300

    button submit added

--- index.js
+++ index.js
@@ -3,17 +3,34 @@ const loadform = async () => {
 	document.body
 	  .appendChild(document.createElement('main'))
 	    .innerHTML = html
-  const [login, password, avatar] = ['login', 'password', 'avatar'].map((id) => document.getElementById(id))
+  const [login, password, avatar, submit] = ['login', 'password', 'avatar', 'submit'].map((id) => document.getElementById(id))
   login.onchange = function(event) {
   	fetch(`http://localhost:3000/users?login=${event.target.value}`)
   	  .then((response) => response.json())
   	  .then((response) => {
   	  	if (response.length === 0) {
   	  	  event.target.style.color = 'green'
+  	  	  password.disabled = false
   	  	} else {
   	  	  event.target.style.color = 'red'
+  	  	  password.disabled = true
   	  	}
   	  })
+  }
+  password.onchange = function(event) {
+
+  }
+  submit.onclick = function(event) {
+  	fetch('http://localhost:3000/users', {
+  		method: 'POST',
+  		headers: {
+  			'Content-Type': 'application/json'
+  		},
+        body: JSON.stringify({
+        	login: login.value,
+        	password: password.value
+        })
+  	}).then((response) => console.log(response.status))
   }
 }
 loadform()
