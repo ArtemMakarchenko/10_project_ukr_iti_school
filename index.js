fddbb2c01f7882db35366ef21ab5df41d126a67e
makarich98 <artem1998makarchenko@gmail.com>
2020-09-08 19:52:27 +0300

    Signup

--- index.js
+++ index.js
@@ -1,48 +1,59 @@
 const loadform = async () => {
-  const html = await (await fetch('https://alina-osyka.github.io/test/resources/form.html')).text()
+	const html = await (await fetch('https://alina-osyka.github.io/test/resources/signup.html')).text()
 	document.body
-	  .appendChild(document.createElement('main'))
-	    .innerHTML = html
-  const [login, password, avatar, submit, picture] = ['login', 'password', 'avatar', 'submit', 'picture'].map((id) => document.getElementById(id))
-  login.onchange = function(event) {
-  	fetch(`http://localhost:3000/users?login=${event.target.value}`)
-  	  .then((response) => response.json())
-  	  .then((response) => {
-  	  	if (response.length === 0) {
-  	  	  event.target.style.color = 'green'
-  	  	  password.disabled = false
-  	  	} else {
-  	  	  event.target.style.color = 'red'
-  	  	  password.disabled = true
-  	  	}
-  	  })
-  }
-  password.onchange = function(event) {
+		.appendChild(document.createElement('main'))
+		.innerHTML = html
+	const [login, password, avatar, submit, picture] = ['login', 'password', 'avatar', 'submit', 'picture']
+		.map((id) => document.getElementById(id))
 
-  }
-  submit.onclick = function(event) {
-  	fetch('http://localhost:3000/users', {
-  		method: 'POST',
-  		headers: {
-  			'Content-Type': 'application/json'
-  		},
-        body: JSON.stringify({
-        	login: login.value,
-        	password: password.value,
-        	avatar: picture.src
-        })
-  	}).then((response) => console.log(response.status))
-  }
-  avatar.onchange = function(event) {
-  	if (event.target.files[0].type.indexOf('image') !== 0) {
-  	  console.warn('Invalid file type')
-  	} else {
-  	  const reader = new FileReader
-  	  reader.onload = function(event) {
-  	  	picture.src = reader.result
-  	  }
-  	  reader.readAsDataURL(event.target.files[0])
-  	}
-  }
+	login.onchange = function (event) {
+		fetch(`http://localhost:3000/users?login=${event.target.value}`)
+			.then((response) => response.json())
+			.then((response) => {
+				if (response.length === 0) {
+					event.target.style.color = 'green'
+					password.disabled = false
+					localStorage.setItem('login', event.target.value)
+				} else {
+					event.target.style.color = 'red'
+					password.disabled = true
+				}
+			})
+	}
+
+	password.onchange = function (event) {
+		if (event.target.length >= 8 && event.target.match(/[\dA-Z]+)/g) {
+			avatar.disabled = false
+			submit.disabled = false
+			const hash = Sha256.hash(event.target.value)
+			localStorage.setItem('pass', hash)
+		}
+	}
+
+	submit.onclick = function (event) {
+		fetch('http://localhost:3000/users', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				login: login.value,
+				password: localStorage.getItem('pass'),
+				avatar: picture.src
+			})
+		}).then((response) => console.log(response.status))
+	}
+
+	avatar.onchange = function (event) {
+		if (event.target.files[0].type.indexOf('image') !== 0) {
+			console.warn('Invalid file type')
+		} else {
+			const reader = new FileReader
+			reader.onload = function (event) {
+				picture.src = reader.result
+			}
+			reader.readAsDataURL(event.target.files[0])
+		}
+	}
 }
 loadform()
